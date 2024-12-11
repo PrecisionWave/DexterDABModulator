8e0051914d1dda90ad81cadb2400566cb3224bb2
Philipp Diethelm <philipp.diethelm@precisionwave.com>
2024-12-11 10:38:56 +0100

    Fixed build tools (mkbootimage)

--- bsp_pcw/get-env.sh
+++ bsp_pcw/get-env.sh
@@ -3,15 +3,18 @@ aarch64-none-linux-gnueabi-static-i686.tar.gz
 arm-none-linux-gnueabi-static-i686.tar.bz2
 uboot-tools-i686-static.tar.gz
 device-tree-compiler-i686-static.tar.gz
-mkbootimage-e3796c3.tar.gz
 )
 
-mkdir -p bin
 cd bin
+
 for i in "${ARCH[@]}"; do
   curl -O "http://www.enclustra.com/binaries/enclustra-bsp/master/${i}"
 done
+
 for i in "${ARCH[@]}"; do
   tar -xf "${i}"
 done
+
+tar -xf mkbootimage-e3796c3.tar.gz
+
 rm "${ARCH[@]}"
