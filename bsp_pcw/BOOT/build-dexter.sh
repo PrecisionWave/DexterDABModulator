eef632ca24a7f74767487b767581a647cc83ece4
Philipp Diethelm <philipp.diethelm@precisionwave.com>
2025-09-22 17:20:03 +0200

    APU: initial microblaze as APU

--- bsp_pcw/BOOT/build-dexter.sh
+++ bsp_pcw/BOOT/build-dexter.sh
@@ -2,6 +2,7 @@
 
 cp ../../hdl_pcw/projects/dexter/dexter/dexter.runs/impl_1/system_top.bit in/zx2/fpga.bit.dexter7020 
 cp ../../hdl_pcw/projects/dexter/dexter/dexter.runs/impl_2/system_top.bit in/zx2/fpga.bit.dexter7010
+cp ../../hdl_pcw/projects/dexter_apu/dexter_apu/dexter_apu.runs/impl_1/system_top.bit in/zx2/fpga.bit.apu
 
 ./build-zx2.sh
 
