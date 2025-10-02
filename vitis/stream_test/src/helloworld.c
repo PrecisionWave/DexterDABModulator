0cb0379aa48401402a1e2d044757ce3fc704450b
Philipp Diethelm <philipp.diethelm@precisionwave.com>
2025-10-02 18:53:54 +0200

    steam_sample: copy data and invert bit pattern

--- vitis/stream_test/src/helloworld.c
+++ vitis/stream_test/src/helloworld.c
@@ -1,50 +1,9 @@
-/******************************************************************************
-*
-* Copyright (C) 2009 - 2014 Xilinx, Inc.  All rights reserved.
-*
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in
-* all copies or substantial portions of the Software.
-*
-* Use of the Software is limited solely to applications:
-* (a) running on a Xilinx device, or
-* (b) that interact with a Xilinx device through a bus or interconnect.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-* XILINX  BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
-* OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-* SOFTWARE.
-*
-* Except as contained in this notice, the name of the Xilinx shall not be used
-* in advertising or otherwise to promote the sale, use or other dealings in
-* this Software without prior written authorization from Xilinx.
-*
-******************************************************************************/
-
-/*
- * helloworld.c: simple test application
- *
- * ------------------------------------------------
- * | UART TYPE   BAUD RATE                        |
- * ------------------------------------------------
- *   uartlite    Configurable only in HW design
- */
-
 #include <stdio.h>
 #include "fsl.h"
 #include "xparameters.h"
 #include "xil_cache.h"
 #include "xil_printf.h"
-
+#include "sleep.h"
 
 
 int main()
@@ -53,12 +12,20 @@ int main()
     Xil_DCacheEnable();
 
     xil_printf("APU Stream test application\n");
-    xil_printf("Built: " __DATE__ __TIME__ "\n");
+    xil_printf("Built: " __DATE__ " " __TIME__ "\n");
 
-    for(;;) {
-		uint32_t value = 0;
-		getfslx(value, 0, FSL_CONTROL);
-		xil_printf("value: %08x\n", value);
+    unsigned counter = 16;
+    uint32_t value;
+
+    for (;;) {
+        getfslx(value, 0, FSL_DEFAULT);
+        putfslx(~value, 0, FSL_DEFAULT);
+        counter--;
+        if (counter == 0) {
+            counter = 16;
+            xil_printf("XFER 16\n");
+            fflush(stdout);
+        }
     }
 
     Xil_DCacheDisable();
