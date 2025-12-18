5aece2396e22607a8d48f8c1ca4858d54323d9d2
Philipp Diethelm <philipp.diethelm@precisionwave.com>
2025-12-18 15:31:10 +0100

    vitis: mmu_test with some results

--- vitis/mmu_test/src/c_main.c
+++ vitis/mmu_test/src/c_main.c
@@ -17,10 +17,22 @@ int main()
     xil_printf("Hello, World from virtual land!\n");
 
     // trigger unaligned pointer exception
-    // *(volatile uint32_t*)(0xbeef) = 0xbadf00d;
+    //  EAR is 0xBEEF
+    //  ESR is 0xC81 (EC=0x01, ESS=0x64, DS=1)
+    //  R17 points to the instruction after the offending store
+     //*(volatile uint32_t*)(0xbeef) = 0xbadf00d;
 
     // trigger null pointer protection
-    *(volatile uint32_t*)(0x8) = 0xbadf00d;
+    //  EAR is 0x8
+    //  ESR is 0x490 (EC=0x10, ESS=0x24, DS=0)
+    //  R17 points to the offending store instruction
+    //*(volatile uint32_t*)(0x8) = 0xbadf00d;
+
+    // trigger null pointer protection
+    //  EAR is 0x10
+    //  ESR is 0x864 (EC = 0x04, ESS = 0x43, DS = 1)
+    //  R17 points to the instruction after the offending load
+    *(volatile uint32_t*)(0x10);
 
     for (;;) {
     };
