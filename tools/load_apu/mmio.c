dacfa01b594da700165a277170ade8452721223b
Philipp Diethelm <philipp.diethelm@precisionwave.com>
2026-01-14 16:45:15 +0100

    load_apu: copy bute for byte...

--- tools/load_apu/mmio.c
+++ tools/load_apu/mmio.c
@@ -3,24 +3,24 @@
 #include "mmio.h"
 
 #include <assert.h>
+#include <string.h>
 
 static inline void MEMORY_BARRIER(void) { asm volatile ("dmb 0xf\ndsb " ::: "memory"); }
 
 
 uint32_t ioread32(void* ptr, size_t reg)
 {
-    volatile uint8_t* p8 = (volatile uint8_t*)(ptr) + reg;
-    volatile uint32_t* p32 = (volatile uint32_t*)p8;
+    uint8_t* p8 = (uint8_t*)(ptr) + reg;
     MEMORY_BARRIER();
-    uint32_t value = *p32;
+    uint32_t value;
+    memcpy(&value, p8, 4);
     return value;
 }
 
 void iowrite32(void* ptr, size_t reg, uint32_t data)
 {
-    volatile uint8_t* p8 = (volatile uint8_t*)(ptr) + reg;
-    volatile uint32_t* p32 = (volatile uint32_t*)p8;
-    *p32 = data;
+    uint8_t* p8 = (uint8_t*)(ptr) + reg;
+    memcpy(p8, &data, 4);
     MEMORY_BARRIER();
 }
 
@@ -29,20 +29,6 @@ void iomemset(void* ptr, size_t reg, uint8_t pattern, size_t length)
     volatile uint8_t* p8 = (volatile uint8_t*)(ptr) + reg;
     volatile uint8_t* p8_end = (volatile uint8_t*)(ptr) + reg + length;
 
-    while ((uintptr_t)p8 & 0x3) {
-        *p8++ = pattern;
-    }
-    if (p8 == p8_end)
-        return;
-
-    volatile uint32_t* p32_end = (volatile uint32_t*)((uintptr_t)p8_end & ~0x3);
-    volatile uint32_t* p32 = (volatile uint32_t*)p8;
-    uint32_t pattern32 = (pattern << 24U) | (pattern << 16U) | (pattern << 8U) | pattern;
-    while (p32 != p32_end) {
-        *p32++ = pattern32;
-    }
-
-    p8 = (volatile uint8_t*)p32;
     while (p8 != p8_end) {
         *p8++ = pattern;
     }
@@ -57,23 +43,9 @@ void copytoio(void* ptr, size_t reg, const void* src, size_t length)
     volatile uint8_t* p8_end = (volatile uint8_t*)(ptr) + reg + length;
     volatile uint8_t* s8 = (volatile uint8_t*)(src);
 
-    while ((uintptr_t)p8 & 0x3) {
-        *p8++ = *s8++;
-    }
-    if (p8 == p8_end)
-        return;
-
-    // copy words
-    volatile uint32_t* p32 = (volatile uint32_t*)p8;
-    volatile uint32_t* p32_end = (volatile uint32_t*)((uintptr_t)p8_end & ~0x3);
-    volatile uint32_t* s32 = (volatile uint32_t*)s8;
-    while (p32 != p32_end) {
-        *p32++ = *s32++;
-    }
-
-    p8 = (volatile uint8_t*)p32;
-    s8 = (volatile uint8_t*)s32;
     while (p8 != p8_end) {
         *p8++ = *s8++;
     }
+
+    MEMORY_BARRIER();
 }
