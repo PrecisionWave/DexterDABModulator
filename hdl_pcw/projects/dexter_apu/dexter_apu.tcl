fe684c3382b3692ccc72f30407adbb05e01b9b7c
Philipp Diethelm <philipp.diethelm@precisionwave.com>
2026-02-05 10:34:36 +0100

    apu: remove unneeded files

--- hdl_pcw/projects/dexter_apu/dexter_apu.tcl
+++ hdl_pcw/projects/dexter_apu/dexter_apu.tcl
@@ -62,9 +62,7 @@ set pcw_xdc_file    system_constr.xdc
 set adi_library     ../../../hdl_adi/library
 set pcw_library     ../../library
 set pcw_hdl_files   [list system_top.v]
-lappend pcw_hdl_files {axi_cache_bypass.vhd}
-lappend pcw_hdl_files {empty_axi.vhd}
-lappend pcw_hdl_files {axi_mpu.vhd}
+
 
 # Use script file location as working directory
 cd [file dirname [file normalize [info script]]]
